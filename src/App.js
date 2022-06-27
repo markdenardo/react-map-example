398470426f1a845d507f9f398889593ad31b1ceb
markdenardo <mark.denardo@gmail.com>
2022-06-27 13:57:11 -0400

    w-list

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
+
+const arr = [
+  "https://www.visitsealife.com/sydney/media/llepm5if/wuru.jpg",
+  "https://education.sakshi.com/sites/default/files/images/2022/02/28/palkbaydugongreserve-1646051325.jpg",
+  "https://conjour.world/wp-content/uploads/Dugong-Sea-Cow-Conservation-Journal-Conjour.jpg",
+];
 
 function App() {
   const [movies, setMovies] = useState([]);
 
   const hook = () => {
-    axios
-      .get('https://web-brut-api.herokuapp.com/movies/')
-      .then(response => {
-        console.log(response)
-        setMovies(response.data)
-      })
-  }
-  
-  useEffect(hook, [])
+    axios.get("https://web-brut-api.herokuapp.com/movies/").then((response) => {
+      console.log(response);
+      setMovies(response.data);
+    });
+  };
+
+  useEffect(hook, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        {movies.map(movie => <li>{movie.name}</li>)}
+        {arr.map((name) => (
+          <li>{name}</li>
+        ))}
       </header>
-      <button onClick={hook}>get movies</button>
     </div>
   );
 }
